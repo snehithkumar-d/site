339192fd2dbe7a33c9b0ae180c4e78f0c65c0785
Snehith Kumar <17601a0530@gmail.com>
2022-02-06 12:10:43 -0800

    Developed Reusable components templates: > Header > Footer
    Signed-off-by: Sunny <17601A0530@gmail.com>

---
+++ Components/footer.js
@@ -0,0 +1,31 @@
+const footerTemplate = document.createElement('template');
+
+footerTemplate.innerHTML = `
+    <link href="/Css/style.css" rel="stylesheet" type="text/css" />
+      <footer>
+         <div class="section-2 footer wf-section">
+            <div class="container-4 w-container">
+                <div class="div-block-2"><p class="paragraph footer-text"><a> Developed by </a> <a href="https://www.linkedin.com/in/snehithkumar/"> Snehith Dhammoju.</a></p></div>
+            </div>
+        </div>
+      </footer>
+`;
+
+class Footer extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const fontAwesome = document.querySelector('link[href*="font-awesome"]');
+        const shadowRoot = this.attachShadow({mode: 'closed'});
+
+        if (fontAwesome) {
+            shadowRoot.appendChild(fontAwesome.cloneNode());
+        }
+
+        shadowRoot.appendChild(footerTemplate.content);
+    }
+}
+
+customElements.define('footer-component', Footer);
